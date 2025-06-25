64ad0c8b7a47eda1b935b524b851a4b7d30f7bd7
hirenjambukiya <161873229+hirenjambukiya@users.noreply.github.com>
2025-06-25 23:35:01 +0530

    Delete chatApp directory

--- chatApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace chatApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
